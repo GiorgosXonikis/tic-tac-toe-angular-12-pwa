018a1af27f60f8ef27d109dbada45899d6c9171b
gxo <georgios.xonikis@gmail.com>
2021-10-23 11:31:07 +0200

    depoloyed to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import {NbButtonModule, NbLayoutModule, NbThemeModule} from '@nebular/theme';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NbEvaIconsModule} from '@nebular/eva-icons';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NbButtonModule,
+        NbLayoutModule,
+        BrowserAnimationsModule,
+        NbThemeModule.forRoot({ name: 'cosmic' }),
+        NbEvaIconsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the app is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
